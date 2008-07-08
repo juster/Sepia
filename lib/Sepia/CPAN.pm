21c55bc3a184b562cbe62781620e38be4a2d817d
Sean O'Rourke <seano@cs.ucla.edu>
2008-07-08 08:52:05 -0700

    group-by-dist

--- lib/Sepia/CPAN.pm
+++ lib/Sepia/CPAN.pm
@@ -18,6 +18,23 @@ sub interesting_parts
     } qw(id cpan_version inst_version fullname cpan_file)};
 }
 
+# only list the "root" module of each package.
+sub group_by_dist
+{
+    my %h;
+    for (@_) {
+        my $cf = $_->{cpan_file};
+        if (exists $h{$cf}) {
+            if (length $h{$cf}{id} > length $_->{id}) {
+                $h{$cf} = $_;
+            }
+        } else {
+            $h{$_->{cpan_file}} = $_;
+        }
+    }
+    sort { $a->{id} cmp $b->{id} } values %h;
+}
+
 sub _list
 {
     CPAN::Shell->expand('Module', shift || '/./');
@@ -25,7 +42,7 @@ sub _list
 
 sub list
 {
-    map { interesting_parts $_ } _list @_
+    group_by_dist map { interesting_parts $_ } _list @_
 }
 
 sub _ls
@@ -39,7 +56,7 @@ sub _ls
 
 sub ls
 {
-    map { interesting_parts $_ } _ls @_
+    group_by_dist map { interesting_parts $_ } _ls @_
 }
 
 sub _desc
@@ -53,7 +70,7 @@ sub _desc
 
 sub desc
 {
-    map { interesting_parts $_ } _desc @_;
+    group_by_dist map { interesting_parts $_ } _desc @_;
 }
 
 sub outdated
@@ -121,7 +138,7 @@ sub _recommend
 
 sub recommend
 {
-    map { interesting_parts $_ } _recommend @_;
+    group_by_dist map { interesting_parts $_ } _recommend @_;
 }
 
 1;
